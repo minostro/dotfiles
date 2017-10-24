6fd8fe7d5af8531320489b021a8728e5653b0be4
Milton Inostroza <me@minostro.com>
2017-10-23 19:35:49 -0700

    Add copy/paste commands

--- emacs-init-local.el
+++ emacs-init-local.el
@@ -1,12 +1,29 @@
 ;;; init-local.el --- My Personal Local Configurations
 ;;
 ;;; Code:
-(xclip-mode 1)
+;;(xclip-mode 1)
 
 (setq org-src-fontify-natively t)
 
 (require 'window-number)
 (window-number-mode)
+
+;; copy/paste starts here
+;; shamelessly copy from http://iancmacdonald.com/macos/emacs/tmux/2017/01/15/macOS-tmux-emacs-copy-past.html
+(defun copy-from-osx ()
+  "Use OSX clipboard to paste."
+  (shell-command-to-string "reattach-to-user-namespace pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  "Add kill ring entries (TEXT) to OSX clipboard.  PUSH."
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+;; copy/paste ends here
 
 (provide 'init-local)
 ;;; init-local.el ends here
