073e628b1df5ffdf7803f9b6a884d4ede524e03c
Milton Inostroza <me@minostro.com>
2017-11-08 08:24:34 -0800

    Add IN PROG state for TODOs and flyspell-mode to Emacs init file

--- emacs-init-local.el
+++ emacs-init-local.el
@@ -59,7 +59,7 @@
 
 ;;; To-do settings
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+      (quote ((sequence "TODO(t)" "NEXT(n)" "IN PROG(i)" "|" "DONE(d!/!)")
               (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
               (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
       org-todo-repeat-to-state "NEXT")
@@ -117,6 +117,9 @@
 
 ;; Adding utf-8 alias
 (define-coding-system-alias 'UTF-8 'utf-8)
+
+;; Enabling flyspell mode
+(flyspell-mode t)
 
 (provide 'init-local)
 ;;; init-local.el ends here
